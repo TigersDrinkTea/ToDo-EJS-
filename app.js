1e871ba9af5374b315d00585bbb73ecbfeecfc78
TigersDrinkTea <46976266+TigersDrinkTea@users.noreply.github.com>
2019-06-03 13:47:12 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ app.get('/', function (req, res) {
     };
     let day = today.toLocaleDateString('en-US', options);
 
-    res.render('list', {listTitle: day, newListItem: items})
+    res.render('list', {
+        listTitle: day,
+        newListItem: items
+    })
 });
 
 
@@ -33,28 +36,34 @@ app.post('/', function (req, res) {
 
     if (req.body.list === 'Work List') {
 
-      workItems.push(item);  
-      res.redirect('/work');
+        workItems.push(item);
+        res.redirect('/work');
 
     } else {
-    
-    items.push(item);
 
-    res.redirect('/');
+        items.push(item);
+
+        res.redirect('/');
     }
 });
 
 app.get('/work', function (req, res) {
-    res.render('list', {listTitle: 'Work List', newListItem: workItems })
-    });
+    res.render('list', {
+        listTitle: 'Work List',
+        newListItem: workItems
+    })
+});
 
+app.get('/about', function (req, res) {
+    res.render('about');
+})
 
 app.post('/', function (req, res) {
     let deleteButton = req.body.delete;
-    deleteButton.addEventListener('click', function(){
-        items.length=0;
+    deleteButton.addEventListener('click', function () {
+        items.length = 0;
     });
- 
+
     res.redirect('/');
 });
 //...................................................................
